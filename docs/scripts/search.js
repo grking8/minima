d5c6eda094faf94a92d01477b3017191ab8d1032
family-guy <grking8@gmail.com>
2017-09-10 09:40:41 +0000

    Get github pages to do build

--- docs/scripts/search.js
+++ docs/scripts/search.js
@@ -1,3 +1,6 @@
+---
+layout: null
+---
 (function () {
 	function getQueryVariable(variable) {
 		var query = window.location.search.substring(1),
@@ -66,7 +69,7 @@
 					contentPreview = getPreview(query, item.content, 170),
 					titlePreview = getPreview(query, item.title);
 
-				resultsHTML += "<li><h4><a href='" + item.url.trim() + "'>" + titlePreview + "</a></h4><p><small>" + contentPreview + "</small></p></li>";
+				resultsHTML += "<li><h4><a href='{{ site.baseurl }}" + item.url.trim() + "'>" + titlePreview + "</a></h4><p><small>" + contentPreview + "</small></p></li>";
 			});
 
 			searchResultsEl.innerHTML = resultsHTML;
