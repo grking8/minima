7e191bc9a0c6f96caf59795a747569f1ca6fdc87
family-guy <guy@zorncapital.com>
2018-04-29 16:23:26 +0100

    Refactor containerise

---
+++ deploy-scripts/scripts/authenticate-gcloud.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set xe
+
+echo "$GCLOUD_SERVICE_KEY" | base64 --decode --ignore-garbage > "$GCLOUD_SERVICE_KEY_PATH"
+gcloud auth activate-service-account --key-file="$GCLOUD_SERVICE_KEY_PATH"
+gcloud config set project "$GCLOUD_PROJECT"
+gcloud config set compute/zone "$GCLOUD_COMPUTE_ZONE"
+gcloud auth configure-docker
+gcloud container clusters get-credentials "$GCLOUD_K8_CLUSTER"
