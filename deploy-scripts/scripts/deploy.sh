e9ff9a4897d10a09c48892b250e6851f36b60b3e
family-guy <guy@zorncapital.com>
2018-05-07 15:25:58 +0100

    Add missing k8 steps

--- deploy-scripts/scripts/deploy.sh
+++ deploy-scripts/scripts/deploy.sh
@@ -4,5 +4,7 @@ set -xe
 
 apt-get -y install gettext-base
 cd deploy-scripts/kubernetes
+kubectl apply -f config-map.yml
 envsubst < deployment.yml | kubectl apply -f -
 envsubst < service.yml | kubectl apply -f -
+envsubst < ingress.yml | kubectl apply -f -
