c95502ec57d237ded383dab1f8acdecdcf2bf438
family-guy <guy@zorncapital.com>
2018-05-07 16:56:44 +0100

    Update deploy script

--- deploy-scripts/scripts/deploy.sh
+++ deploy-scripts/scripts/deploy.sh
@@ -4,6 +4,8 @@ set -xe
 
 apt-get -y install gettext-base
 cd deploy-scripts/kubernetes
+kubectl apply -f config-map.yml
+envsubst < secrets.yml | kubectl apply -f -
 envsubst < deployment.yml | kubectl apply -f -
 envsubst < service.yml | kubectl apply -f -
 envsubst < ingress.yml | kubectl apply -f -
