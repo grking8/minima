a188de333b9c2c06f23be6ae10f173585a5e693c
family-guy <guy@zorncapital.com>
2018-05-07 19:20:04 +0100

    Use http not tcp load balancer

--- deploy-scripts/scripts/deploy.sh
+++ deploy-scripts/scripts/deploy.sh
@@ -4,5 +4,6 @@ set -xe
 
 apt-get -y install gettext-base
 cd deploy-scripts/kubernetes
+envsubst < ingress.yml | kubectl apply -f -
 envsubst < deployment.yml | kubectl apply -f -
 envsubst < service.yml | kubectl apply -f -
