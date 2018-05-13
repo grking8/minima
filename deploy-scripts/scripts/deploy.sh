8b1221195da00357927d8fb48c62d22b87fe2eb9
family-guy <guy@zorncapital.com>
2018-05-13 13:19:05 +0100

    Change lb from tcp to http

--- deploy-scripts/scripts/deploy.sh
+++ deploy-scripts/scripts/deploy.sh
@@ -6,3 +6,5 @@ apt-get -y install gettext-base
 cd deploy-scripts/kubernetes
 envsubst < deployment.yml | kubectl apply -f -
 envsubst < service.yml | kubectl apply -f -
+envsubst < ingress.yml | kubectl apply -f -
+
