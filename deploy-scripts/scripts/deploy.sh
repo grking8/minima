c399dc5b4c1c1518bdf57dadd1be4414413aaef5
family-guy <guy@zorncapital.com>
2018-07-01 18:39:35 +0100

    Use create

--- deploy-scripts/scripts/deploy.sh
+++ deploy-scripts/scripts/deploy.sh
@@ -4,7 +4,7 @@ set -xe
 
 apt-get -y install gettext-base
 cd deploy-scripts/kubernetes
-kubectl apply -f config-map.yml
+kubectl create -f config-map.yml
 envsubst < secrets.yml | kubectl apply -f -
 envsubst < deployment.yml | kubectl apply -f -
 envsubst < service.yml | kubectl apply -f -
