595d14f7947a709aaee91d6c7f8dc48c0a15c55c
family-guy <guy@zorncapital.com>
2018-07-18 17:30:20 +0100

    Correct docker image names

--- deploy-homepage.sh
+++ deploy-homepage.sh
@@ -3,8 +3,8 @@
 CONTAINER=homepage
 docker container stop $CONTAINER
 docker rm $CONTAINER
-docker pull guydocker/minima
+docker pull guydocker/minima:gce
 docker run --detach --name=$CONTAINER --restart=always --publish=80:80 \
 	--publish=443:443 --volume=/etc/letsencrypt/:/etc/letsencrypt/ \
-	--volume=/etc/nginx/:/etc/nginx/ guydocker/minima
+	--volume=/etc/nginx/:/etc/nginx/ guydocker/minima:gce
 docker rmi $(docker images --filter "dangling=true" --quiet)
